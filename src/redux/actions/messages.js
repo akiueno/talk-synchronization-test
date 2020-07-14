38b08030bb85ea6f2a83a945d7d5d1fa4384dd8a
Akihiro Ueno <akihiro.ueno.2@gmail.com>
2020-07-14 13:18:18 +0900

    first commit

---
+++ src/redux/actions/messages.js
@@ -0,0 +1,36 @@
+import {createActions} from 'redux-actions';
+import {firebaseDb} from '../../firebase';
+
+const messagesRef = firebaseDb.ref('messages');
+
+const {messages} = createActions({
+  messages: {
+    submit(value, image) {
+      const payload = value;
+
+      console.log('ボタンが押されました');
+
+      if (payload === '') {
+        alert('メッセージを入力してください');
+        return false;
+      }
+
+      console.log('ボタンが押されました')
+
+      messagesRef.push({
+        image: image,
+        text: payload,
+      });
+
+      return payload;
+    },
+    change(value) {
+      const payload = value;
+      return payload;
+    },
+  },
+});
+
+export {messages};
+
+//inputの入力値はComponentで管理したほうがいい
---
+++ src/redux/store/initialState/messages.js
@@ -0,0 +1,7 @@
+export const messages = {
+  value: '',
+  text: 'テスト',
+  image:
+    'https://secretldn.com/wp-content/uploads/2017/07/micropig-feature2.jpg',
+  msgs: [],
+};
